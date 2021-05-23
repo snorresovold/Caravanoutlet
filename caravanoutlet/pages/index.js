9480369dd93db558013f3b160817200fd31b722e
snorresovold <73354016+snorresovold@users.noreply.github.com>
2021-05-23 23:23:15 +0200

    updates

---
+++ caravanoutlet/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+import { commerce } from '../lib/commerce'
+
+export default function Home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
