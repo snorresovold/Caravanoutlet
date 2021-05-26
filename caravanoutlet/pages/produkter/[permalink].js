cb09dbf65ffc14ce4db75df4a1b8dbd1599bbdc3
snorresovold <73354016+snorresovold@users.noreply.github.com>
2021-05-26 22:33:58 +0200

    Update [permalink].js

---
+++ caravanoutlet/pages/produkter/[permalink].js
@@ -0,0 +1,58 @@
+import React from "react";
+import Head from "next/head";
+
+import { commerce } from "../../lib/commerce";
+import { useCartDispatch } from "../../context/cart";
+import { useThemeDispatch } from "../../context/theme";
+import { useModalDispatch } from "../../context/modal";
+
+export async function getStaticProps({ params }) {
+    const { permalink } = params;
+  
+    const product = await commerce.products.retrieve(permalink, {
+      type: "permalink",
+    });
+  
+    return {
+      props: {
+        product,
+      },
+      revalidate: 60,
+    };
+  }
+  
+  export async function getStaticPaths() {
+    const { data: products } = await commerce.products.list();
+  
+    return {
+      paths: products.map(({ permalink }) => ({
+        params: {
+          permalink,
+        },
+      })),
+      fallback: false,
+    };
+  }
+
+function productPage({ ptoduct }) {
+    const { setCart } = useCartDispatch();
+    const {
+      variant_groups: variantGroups,
+      assets,
+      meta,
+      related_products: relatedProducts,
+    } = product;
+    const images = assets.filter(({ is_image }) => is_image);
+    const setTheme = useThemeDispatch();
+    const { openModal } = useModalDispatch();
+
+    const initialVariants = React.useMemo(
+        () =>
+          variantGroups.reduce((all, { id, options }) => {
+            const [firstOption] = options;
+    
+            return { ...all, [id]: firstOption.id };
+          }, {}),
+        [product.permalink]
+      );
+}
